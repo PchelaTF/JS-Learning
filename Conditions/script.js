2fd00ef5dcf4c7a39c3ea8ba96cb818c24c85298
PchelaTF <PchelaTF@gmail.com>
2022-05-16 15:41:13 +0300

    Complete Conditions

--- Conditions/script.js
+++ Conditions/script.js
@@ -1,2 +1,53 @@
 "use strict";
 
+if (4 == 9){
+    console.log('OK!');
+} else {
+    console.log('ERROR');
+}
+// всегда возвращет булевое значение (true or false)
+
+const num = 50;
+
+// if (num < 49){
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Too mach');
+// } else {
+//     console.log('Ok');
+// }
+//
+// (num === 50) ? console.log('Ok') : console.log('Error');
+// // тернарный оператор ?
+
+switch (num) {
+    case 49:
+        console.log('Error');
+        break;
+    case 100:
+        console.log('Error');
+        break;
+    case 51:
+        console.log("Ok");
+        break;
+    default:
+        console.log("Maybe another time");
+        break;
+}
+
+const num2 = '50';
+
+switch (num2) {
+    case '49':
+        console.log('Error');
+        break;
+    case '100':
+        console.log('Error');
+        break;
+    case '50':
+        console.log("Ok");
+        break;
+    default:
+        console.log("Maybe another time");
+        break;
+}
