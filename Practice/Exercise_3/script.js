dbf80272f4d195f455a0b739a2b82f892e19f7a9
PchelaTF <PchelaTF@gmail.com>
2022-05-21 18:20:31 +0300

    .trim() in Exercise_3

--- Practice/Exercise_3/script.js
+++ Practice/Exercise_3/script.js
@@ -49,8 +49,8 @@ const personalMovieDB = {
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++){
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = prompt('На сколько оцените его?', '');
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+            b = prompt('На сколько оцените его?', '').trim();
 
         if (a !== '' && b !== '' && a != null && b != null && a.length < 50 && b.length < 50){
             personalMovieDB.movies[a] = b;
