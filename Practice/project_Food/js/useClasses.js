11c2e3939a018a39de6bc8a5c3c848991b4194ea
PchelaTF <PchelaTF@gmail.com>
2022-06-05 13:10:35 +0300

    Replased Practice, Add Rest, Add rest in useClasses in project_Food

--- Practice/project_Food/js/useClasses.js
+++ Practice/project_Food/js/useClasses.js
@@ -5,12 +5,13 @@ window.addEventListener('DOMContentLoaded', () => {
     // будем использовать классы для карточек 
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) { // добавим сюда rest оператор
             this.src = src;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
             this.price = price;
+            this.classes = classes; // это будезт массив
             this.parentSelector = document.querySelector(parentSelector);
             this.transfer = 35;
             this.changeToUAN();
@@ -22,8 +23,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+
+            // довами класс по умолчанию. Для случаев если он не будет передан в аргумент
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className)); // добавление к элементу(div на 25й) и добавляем к нему каждый класс который будет нажодится в массиве
+            }
+
+            // отредактируем верстку чуть-чуть
             element.innerHTML = `
-            <div class="menu__item">
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -32,7 +42,6 @@ window.addEventListener('DOMContentLoaded', () => {
                     <div class="menu__item-cost">Цена:</div>
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
-            </div>
             `;
 
             this.parentSelector.append(element);
@@ -57,7 +66,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         14,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
 
     new MenuCard(
@@ -66,6 +76,7 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         21,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render();
 });
